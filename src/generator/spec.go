ef786a451c3795d1bdb2bb3775ed16296da7a16c
Radon10043 <772577952@qq.com>
2025-12-15 19:43:06 +0800

    src/generator: add missing bracket

--- src/generator/spec.go
+++ src/generator/spec.go
@@ -167,7 +167,7 @@ func execWriteStep(
 	kAgent *agent.Agent, sysPromptMap *map[string]string, gvEntry *database.GlobalVar, cfg *ProgConfig, wsh *writeSpecHelper,
 ) error {
 	var logger *log.Logger
-	logger = log.New(os.Stdout, wsh.LogPrefix+gvEntry.Name+"]["+wsh.Next+"] ", log.LstdFlags|log.Lmsgprefix)
+	logger = log.New(os.Stdout, wsh.LogPrefix+"["+gvEntry.Name+"]["+wsh.Next+"] ", log.LstdFlags|log.Lmsgprefix)
 	switch wsh.Next {
 	case "outline":
 		return execOutlineStep(kAgent, (*sysPromptMap)["outline"], gvEntry, logger, wsh)
