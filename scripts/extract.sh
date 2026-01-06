fd7380090ab21b84570d6ab2e558ce92048e47c9
Radon10043 <772577952@qq.com>
2026-01-06 21:42:32 +0800

    scripts: add extract.sh for constant value extraction

---
+++ scripts/extract.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+KERNEL=  # Path to kernel source tree, e.g. /vol/linux/v6.12-extract
+
+ARCH=386,amd64,arm,arm64,mips64le,ppc64le,riscv64,s390x
+if [ ! -z $1 ]; then
+    ARCH=$1
+fi
+
+$(dirname $0)/bin/syz-extract -build -sourcedir=$KERNEL -os=linux -includedirs=$KERNEL/fs/xfs/libxfs -arch=$ARCH
