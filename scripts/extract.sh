b00ccae579f3d7b8aef618f5323d368694292e8b
Radon10043 <772577952@qq.com>
2026-01-07 15:37:29 +0800

    scripts: add usage for extract.sh

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-KERNEL=  # Path to kernel source tree, e.g. /vol/linux/v6.12-extract
+# this scripts is written for spec debugging
+#
+# usage:
+#   KERNEL=/path/to/kernel-for-extract $CLOUD/scripts/extract.sh $ARCH
+#
+# if $ARCH is not set, extract constants for all arches.
+#
+# example:
+#   KERNEL=/vol/linux/v6.12-extract $CLOUD/scripts/extract.sh amd64
 
 ARCH=386,amd64,arm,arm64,mips64le,ppc64le,riscv64,s390x
 if [ ! -z $1 ]; then
