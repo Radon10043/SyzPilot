12f78ec9b66f2d24c2b9985f7e71387746f2e95d
Radon10043 <772577952@qq.com>
2026-02-14 20:52:17 +0800

    scripts: update arg parsing in logs.sh

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -4,14 +4,49 @@
 # of syzkaller crash logs
 #
 # usage:
-#   bash scripts/logs.sh <CRASH_PATH> <INTEREST_CRASH_FILE>
+#   bash scripts/logs.sh -d <CRASH_DIR> -i <INTEREST_CRASH>
 
-CRASH_PATH=$(realpath $1)
-INTEREST_CRASH=$(realpath $2)
+set -euo pipefail
+
+# required args
+CRASH_DIR=
+INTEREST_CRASH=
+
+print_help() {
+    echo "usage: $0 [ARGS]"
+    echo "  args (required):"
+    echo "    -d,--dir <DIR>            path to the directory containing syzkaller crash logs"
+    echo "    -i,--interest <INTEREST>  path to the crash file that contains the titles of the crashes we are interested in, one title per line"
+    echo "  args (optional):"
+    echo "    -h, --help                print help message"
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -d | --dir)
+        CRASH_DIR=$(realpath $2)
+        shift
+        shift
+        ;;
+    -i | --interest)
+        INTEREST_CRASH=$(realpath $2)
+        shift
+        shift
+        ;;
+    -h | --help)
+        print_help
+        exit 0
+        ;;
+    *)
+        echo "unknown arg: $1"
+        exit 1
+        ;;
+    esac
+done
 
 logs=()
 while IFS= read -r title; do
-    desc=$(find $CRASH_PATH -name description -exec grep -l "$title" {} +)
+    desc=$(find $CRASH_DIR -name description -exec grep -l "$title" {} +)
     new_logs=$(find $(dirname $desc) -name "log*")
     logs+=("${new_logs[@]}")
 done < "$INTEREST_CRASH"
