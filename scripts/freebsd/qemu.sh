9770dfb3e33bd0a4e46d4510308bf4b9e9c84a86
Radon10043 <772577952@qq.com>
2026-03-16 13:03:44 +0800

    scripts/freebsd: add qemu.sh and exec-syz.sh

---
+++ scripts/freebsd/qemu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# qemu command used by syzkaller, for debugging
+#
+# usage:
+#   IMAGE=/path/to/image-under-test.qcow2 $CLOUD/scripts/freebsd/qemu.sh
+
+qemu-system-x86_64 \
+    -m 16384 \
+    -smp 4 \
+    -chardev socket,id=SOCKSYZ,server=on,wait=off,host=localhost,port=46821 \
+    -mon chardev=SOCKSYZ,mode=control \
+    -display none \
+    -serial stdio \
+    -no-reboot \
+    -device virtio-rng-pci \
+    -enable-kvm \
+    -device e1000,netdev=net0 \
+    -netdev user,id=net0,restrict=on,hostfwd=tcp:127.0.0.1:3733-:22 \
+    -hda $IMAGE \
+    -snapshot
