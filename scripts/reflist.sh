0bffb0cb9c56041f5ce2923c20fd2d256cf32def
Radon10043 <772577952@qq.com>
2026-05-08 17:19:22 +0800

    scripts: update reflist.sh

--- scripts/reflist.sh
+++ scripts/reflist.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-for f in $(find $1 -name .tqueue); do basename $(dirname $f) | cut -d# -f1; done | sort
+for f in $(find $1 -name .tqueue); do
+    basename $(dirname $f) | cut -d# -f1 | sed 's/^/variable,/'
+done | sort
