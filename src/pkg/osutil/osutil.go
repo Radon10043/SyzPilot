c7107833423b8528c05cde78196146f13dd6f84a
Radon10043 <772577952@qq.com>
2026-05-10 21:31:31 +0800

    src: update kernel file path and flag descriptions

--- src/pkg/osutil/osutil.go
+++ src/pkg/osutil/osutil.go
@@ -39,11 +39,11 @@ func (o OsType) KernFilePath(prefix string) string {
 	case Linux:
 		return filepath.Join(prefix, "vmlinux")
 	case FreeBSD:
-		return filepath.Join(prefix, "sys", runtime.GOARCH, "compile", "CLOUD", "kernel.full")
+		return filepath.Join(prefix, "sys", runtime.GOARCH, "compile", "SYZPILOT", "kernel.full")
 	case OpenBSD:
-		return filepath.Join(prefix, "sys", "arch", runtime.GOARCH, "compile", "CLOUD", "obj", "bsd.gdb")
+		return filepath.Join(prefix, "sys", "arch", runtime.GOARCH, "compile", "SYZPILOT", "obj", "bsd.gdb")
 	case NetBSD:
-		return filepath.Join(prefix, "sys", "arch", runtime.GOARCH, "compile", "obj", "CLOUD", "netbsd.gdb")
+		return filepath.Join(prefix, "sys", "arch", runtime.GOARCH, "compile", "obj", "SYZPILOT", "netbsd.gdb")
 	default:
 		return ""
 	}
